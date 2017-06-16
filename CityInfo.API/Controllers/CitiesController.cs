ff1ffdf4672199aeb9d67105f6bbb068c6a40fd2
lmcfsgit <luis.mcf.silva@gmail.com>
2017-06-16 18:56:32 +0100

    Add project files.

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Models;
+
+namespace CityInfo.API.Controllers
+{
+
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+
+        public IActionResult GetCities()
+        {
+
+            return Ok(CitiesDataStore.Current.Cities); 
+            //return new JsonResult(new List<object>()
+            //{
+            //    new { id=1, Name="New York"},
+            //    new { id=2, Name="Antwerp"}
+            //});
+    
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            // find city
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+
+        }
+
+    }
+}
